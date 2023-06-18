8cb44e2538305fa20abe53d8622e3c9cf87f2adc
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-19 00:46:32 +0330

    Start of project

--- weather.py
+++ weather.py
@@ -1,11 +1,15 @@
 import json
 
+temp_date_com = []
+
 with open("data.json") as file:
     data = json.load(file)
     temp_data = data['result']['list']
 
 for temp in temp_data:
-    if temp['main']["temp_max"] > 36:
+    if temp['main']["temp_max"] > 20:
         temp_date = temp["dt_txt"].split(":")[0][0:10]
         day_temp = temp["main"]["temp"]
-        print(f"{temp_date}: {day_temp}")
+        temp_date_com.append({temp_date: day_temp})
+
+print(temp_date_com)
